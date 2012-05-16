5776d4ac2d6061eb7baa785780bc44023554e5a5
Joseph Chiang <joseph_chiang@miiicasa.com>
2012-05-16 23:29:35 +0800

    Add API document.

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ScrollPagination"
+    ],
+    "modules": [
+        "scroll-pagination"
+    ],
+    "allModules": [
+        {
+            "displayName": "scroll-pagination",
+            "name": "scroll-pagination",
+            "description": "A widget that makes a page or a division can dynamically load data\nby mouse scrolling without clicking any links.\nIt has various namings such as Continuous Scrolling, Infinite Scroll,\nand the name we use, Scroll Pagination."
+        }
+    ]
+} };
+});
